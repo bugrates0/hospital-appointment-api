2ec8dc677235a383e97be588e1f948a91c2e4d25
Bugra Ates <bugrates25@gmail.com>
2024-03-09 12:38:50 +0300

    project initialized

---
+++ src/main/java/com/bugrates/HospitalAppointmentBookingAPI/HospitalAppointmentBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.bugrates.HospitalAppointmentBookingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAppointmentBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAppointmentBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bugrates/HospitalAppointmentBookingAPI/HospitalAppointmentBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugrates.HospitalAppointmentBookingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
